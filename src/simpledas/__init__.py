64c6ffea2d92828f645da24368f764fcee294852
qgoestch <quentin.goestchel@ens-paris-saclay.fr>
2025-03-06 16:04:30 -0800

    Fixed deprecated key

--- src/simpledas/__init__.py
+++ src/simpledas/__init__.py
@@ -3,5 +3,13 @@ from . import h5pydict
 import importlib.metadata
 
 __all__ = ['simpleDASreader', 'h5pydict']
-__version__ = importlib.metadata.version(__name__)
-__author__ = importlib.metadata.metadata('simpledas')['Author']
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
+try:
+    __author__ = importlib.metadata.get('Author', 'Ole Henrik Waagaard')
+except (importlib.metadata.PackageNotFoundError, KeyError):
+    __author__ = "unknown"
