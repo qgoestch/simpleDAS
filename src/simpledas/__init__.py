cd41fc4a24af2121c07c77d023c877e018ffa393
qgoestch <quentin.goestchel@ens-paris-saclay.fr>
2025-03-07 09:12:58 -0800

    Changed importlib

--- src/simpledas/__init__.py
+++ src/simpledas/__init__.py
@@ -10,6 +10,6 @@ except importlib.metadata.PackageNotFoundError:
     __version__ = "unknown"
 
 try:
-    __author__ = importlib.metadata.get('Author', 'Ole Henrik Waagaard')
-except (importlib.metadata.PackageNotFoundError, KeyError):
-    __author__ = "unknown"
+    __author__ = importlib.metadata.author(__name__)
+except:
+    __author__ = "Ole Henrik Waagaard"
